4e0beb5cb2ce4cd7486f7f469657f274ddd067f0
shawkyelshalawy <shawkyfariedelshalawy@gmail.com>
2025-02-16 15:33:04 +0200

    refactor URL storage in Redis to use hashes and add logging for redirect requests

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -2,12 +2,13 @@ package app
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jaevor/go-nanoid"
+	"go.uber.org/zap"
 )
-const urlTTL = 30 * 24 * time.Hour
 
 func (app *Application) ShortenHandler(c *gin.Context) {
 	var req struct {
@@ -20,7 +21,7 @@ func (app *Application) ShortenHandler(c *gin.Context) {
 	}
 
 	
-	existing, err := app.Redis.Get(c, "long:"+req.URL).Result()
+	existing, err := app.Redis.HGet(c, os.Getenv("shortenedUrlsHash"), req.URL).Result()
 	if err == nil && existing != "" {
 		c.JSON(http.StatusOK, gin.H{"short_url": app.Config.BaseURL + "/" + existing})
 		return
@@ -30,25 +31,39 @@ func (app *Application) ShortenHandler(c *gin.Context) {
 	gen, _ := nanoid.Standard(6)
 	shortID := gen()
 
-	
 	pipe := app.Redis.TxPipeline()
-	pipe.Set(c, "short:"+shortID, req.URL,urlTTL)
-	pipe.Set(c, "long:"+req.URL, shortID, urlTTL)
+	pipe.HSet(c,  os.Getenv("shortenedUrlsHash"), shortID, req.URL)
+	pipe.HSet(c, os.Getenv("originalUrlsHash"), req.URL, shortID)
+	
+	pipe.Expire(c, os.Getenv("shortenedUrlsHash"), 30*24*time.Hour)
+	pipe.Expire(c, os.Getenv("originalUrlsHash"), 30*24*time.Hour)
+
 	if _, err := pipe.Exec(c); err != nil {
 		app.errorResponse(c, http.StatusInternalServerError, "Failed to create short URL")
 		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
-		"short_url": app.Config.BaseURL + "/" + shortID,
+		"short_url":  app.Config.BaseURL + "/" + shortID,
 		"expires_at": time.Now().Add(30 * 24 * time.Hour),
 	})
 }
 
 func (app *Application) RedirectHandler(c *gin.Context) {
 	shortID := c.Param("shortID")
-	url, err := app.Redis.Get(c, "short:"+shortID).Result()
+	
+	app.Logger.Info("Redirect request received",
+		zap.String("shortID", shortID),
+		zap.String("path", c.Request.URL.Path),
+	)
+
+	
+	url, err := app.Redis.HGet(c, os.Getenv("shortenedUrlsHash"), shortID).Result()
 	if err != nil {
+		app.Logger.Error("Redis get error",
+			zap.String("shortID", shortID),
+			zap.Error(err),
+		)
 		app.errorResponse(c, http.StatusNotFound, "URL not found")
 		return
 	}
